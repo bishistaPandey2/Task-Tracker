994322c60bc22ffc2a5fb95c76ba32014b59855f
bishistaPandey2 <bishistapandey@gmail.com>
2026-03-21 19:57:53 +0545

    Complete functionality of update cli.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ readlineSync.promptCLLoop({
   add: (description) => {
     let tasks = [];
     
+    //Extracting or reading data from file.
     if(fs.existsSync('todo.json')){
       const data = fs.readFileSync('todo.json', 'utf8');
       tasks = JSON.parse(data);    
     }
+
+    //Creating an array for todo.
     const todo =[];
      
+    // If task already exists pushing it back.
     if(tasks.length !== 0){
       let i;
       for(i = 0; i<tasks.length; i++){
@@ -19,6 +23,7 @@ readlineSync.promptCLLoop({
       }
     };
 
+    // Pushing new tasks.
     todo.push(
       {
         id: undefined,
@@ -29,12 +34,14 @@ readlineSync.promptCLLoop({
       } 
     );
 
+    //providing id to those tasks.
     todo.map(element => {
       for(let i = 0; i<todo.length; i++){
         todo[i].id = i+1;
       }
     });
 
+    //Writing it in the file.
     fs.writeFile('todo.json', JSON.stringify(todo, null, 2), 'utf8', 
       (error) => {
          if(error) {
@@ -42,6 +49,34 @@ readlineSync.promptCLLoop({
         }
           console.log("Task written successfully.");
      });
+
+    return true;
+  },
+
+  update: (id, description) => {
+    let tasks = [];
+
+    //Extracting data from the file.
+    if(fs.existsSync('todo.json')){
+      const data = fs.readFileSync('todo.json', 'utf8');
+      tasks = JSON.parse(data);
+    }
+
+    //If data exista then appending.
+    if(tasks.length !== 0){
+      const actualId = id-1;
+      tasks[actualId].description = description;
+      tasks[actualId].updatedAt = new Date();
+    }
+
+    // rewriting the appended data in the file. 
+    fs.writeFile('todo.json', JSON.stringify(tasks, null, 2), 'utf8',
+      (error) => {
+        if(error) {
+          console.log('Problem while writing filr.', error);
+        }
+          console.log("Task updated successfully");
+    });
 
     return true;
   }
