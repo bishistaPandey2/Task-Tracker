3ec86e3c117632e6efd67768bac1d0339ea17be7
bishistaPandey2 <bishistapandey@gmail.com>
2026-03-21 18:15:29 +0545

    Complete add cli task.

--- src/index.js
+++ src/index.js
@@ -10,14 +10,7 @@ readlineSync.promptCLLoop({
       const data = fs.readFileSync('todo.json', 'utf8');
       tasks = JSON.parse(data);    
     }
-    const todo =[ 
-      {
-        description,
-        status: "todo",
-        createdAt: new Date,
-        updatedAt: new Date
-      } 
-    ];
+    const todo =[];
      
     if(tasks.length !== 0){
       let i;
@@ -26,14 +19,30 @@ readlineSync.promptCLLoop({
       }
     };
 
+    todo.push(
+      {
+        id: undefined,
+        description,
+        status: "todo",
+        createdAt: new Date,
+        updatedAt: new Date
+      } 
+    );
+
+    todo.map(element => {
+      for(let i = 0; i<todo.length; i++){
+        todo[i].id = i+1;
+      }
+    });
+
     fs.writeFile('todo.json', JSON.stringify(todo, null, 2), 'utf8', 
       (error) => {
          if(error) {
-         console.log('problem while writing in file.', error);
-       }
-        console.log("Task written successfully.")
-     })
+          console.log('problem while writing in file.', error);
+        }
+          console.log("Task written successfully.");
+     });
+
     return true;
   }
 });
-
