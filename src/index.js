7de521f0084bad0b848b59275e5761c097223d76
bishistaPandey2 <bishistapandey@gmail.com>
2026-03-23 09:17:53 +0545

    Complete the cli functionality of delete and do some subtle changes.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ readlineSync.promptCLLoop({
     let tasks = [];
     
     //Extracting or reading data from file.
-    if(fs.existsSync('todo.json')){
-      const data = fs.readFileSync('todo.json', 'utf8');
-      tasks = JSON.parse(data);    
+    if(fs.existsSync('../todo.json')){
+      const data = fs.readFileSync('../todo.json', 'utf8');
+      if(data !== undefined){
+        tasks = JSON.parse(data);
+      }
     }
 
     //Creating an array for todo.
@@ -42,7 +44,7 @@ readlineSync.promptCLLoop({
     });
 
     //Writing it in the file.
-    fs.writeFile('todo.json', JSON.stringify(todo, null, 2), 'utf8', 
+    fs.writeFile('../todo.json', JSON.stringify(todo, null, 2), 'utf8', 
       (error) => {
          if(error) {
           console.log('problem while writing in file.', error);
@@ -54,29 +56,83 @@ readlineSync.promptCLLoop({
   },
 
   update: (id, description) => {
-    let tasks = [];
 
-    //Extracting data from the file.
-    if(fs.existsSync('todo.json')){
-      const data = fs.readFileSync('todo.json', 'utf8');
-      tasks = JSON.parse(data);
+    if(parseInt(id) < 0 || parseInt(id) === 0){
+      console.log("Please provide correct Id.")
     }
 
-    //If data exista then appending.
-    if(tasks.length !== 0){
-      const actualId = id-1;
-      tasks[actualId].description = description;
-      tasks[actualId].updatedAt = new Date();
-    }
+    if(parseInt(id) > 0 && parseInt(id) !== 0){
 
-    // rewriting the appended data in the file. 
-    fs.writeFile('todo.json', JSON.stringify(tasks, null, 2), 'utf8',
-      (error) => {
-        if(error) {
-          console.log('Problem while writing filr.', error);
+      let tasks = [];
+
+      //Extracting data from the file.
+      if(fs.existsSync('../todo.json')){
+        const data = fs.readFileSync('../todo.json', 'utf8');
+        if(data !== undefined){
+          tasks = JSON.parse(data);
         }
-          console.log("Task updated successfully");
-    });
+      }
+
+      //If data exista then appending.
+      if(tasks.length !== 0){
+        const actualId = parseInt(id) - 1;
+        tasks[actualId].description = description;
+        tasks[actualId].updatedAt = new Date();
+      }
+
+      // rewriting the appended data in the file. 
+      fs.writeFile('../todo.json', JSON.stringify(tasks, null, 2), 'utf8',
+       (error) => {
+         if(error) {
+           console.log('Problem while writing file.', error);
+         }
+            console.log("Task updated successfully");
+      });
+    }
+
+    return true;
+  },
+
+  delete: (id) => {
+
+    if(parseInt(id) < 0 || parseInt(id) === 0){
+      console.log("Please provide correct Id.")
+    }
+
+    if(parseInt(id) > 0 && parseInt(id) !== 0){
+      let tasks = [];
+
+      //Read from the file if the file exists.
+      if(fs.existsSync('../todo.json')){
+        const data = fs.readFileSync('../todo.json', 'utf8');
+        if(data !== undefined){  //Runs only if there is something in the box.
+          tasks = JSON.parse(data);
+        }
+      };
+
+      //Matching the use given id into the ) order array element.
+      let actualId = parseInt(id) - 1;
+
+      //Removing from the array of task objects.
+      if(tasks.length !== 0){
+        tasks.splice(actualId, 1);
+      };
+
+      //Re-assigining new ids to the elements or objects.
+      for(let i = 0; i<tasks.length; i++){
+        tasks[i].id = i+1;
+      }
+
+
+      //Finally writing th data to the file.
+      fs.writeFile('../todo.json', JSON.stringify(tasks, null, 2), 'utf8', (error) => {
+        if(error){
+          console.log("Problem while deleting from file.")
+        }
+
+        console.log(`Id:${id} is successfully deleted from the file.`)
+      });
+    }
 
     return true;
   }
